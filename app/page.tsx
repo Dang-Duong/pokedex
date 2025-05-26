fd93c2b9294db99f1068809fb263da7d4abc69f7
David Nguyen <duongd973@gmail.com>
2025-05-26 22:38:57 +0200

    feat: add background

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Background } from "./components/background/Background";
 import { Header } from "./components/header/Header";
 
 export default function Home() {
-  return <Header />;
+  return (
+    <>
+      <Background />
+      <Header />
+    </>
+  );
 }
